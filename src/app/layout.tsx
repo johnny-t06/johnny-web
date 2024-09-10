647e407fde5d1e3cc1ec6ccee1c43f7008901120
Johnny Tan <jonnytan0818@gmail.com>
2024-09-09 22:50:53 -0400

    Update icon and description metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
 export const metadata: Metadata = {
   title: "Johnny Tan",
+  description: "Johnny Tan's personal website",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
